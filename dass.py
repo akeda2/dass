9fc46fdc29fe9b96dd54de91d083d1c0959464ab
akeda <akeda2@gmail.com>
2023-02-17 14:18:00 +0100

    initial commit

---
+++ dass.py
@@ -0,0 +1,23 @@
+# SHort program that concatenates text files in directories and subdirectories into one file.
+# Usage: python dass.py [directory] [output file]
+# The output file will be created if it does not exist. If it does exist, it will be overwritten.
+
+import os
+import sys
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python dass.py [directory] [output file]")
+        return
+    directory = sys.argv[1]
+    output_file = sys.argv[2]
+    output = open(output_file, 'w')
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".txt"):
+                input_file = open(os.path.join(root, file), 'r')
+                output.write(input_file.read())
+                output.write("...")
+                input_file.close()
+    output.close()
+    
