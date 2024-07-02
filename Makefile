1ee98f56d6c0ac3b48d871737a040b468f423afe
akeda <akeda2@gmail.com>
2024-07-02 17:13:35 +0200

    update build.sh

--- Makefile
+++ Makefile
@@ -6,9 +6,17 @@ all: dass del
 
 # Create dass executable
 dass:
-	pyinstaller dass.py -F
-	mv dist/dass .
+	# Check for python3-venv:
+	dpkg -l | grep -q python3-venv || echo "python3-venv package not installed'"
+	python3 -m venv --help &> /dev/null || { echo "Python venv is not installed!'"; exit 1; }
+	# Create venv if not exists
+	[[ ! -d venv ]] && { python3 -m venv venv || { echo "venv creation failed" ; exit 1; } ; }
+	source venv/bin/activate || { echo "venv activation failed" ; exit 1; }
+	echo "Installing dependenies"
+	pip install -r requirements.txt || { echo "pip install failed" ; exit 1; }
 
+	pyinstaller --onefile "dass".py --clean -F --noupx && mv dist/dass . || { echo "pyinstaller failed" ; exit 1; }
+	deactivate || { echo "venv deactivation failed" ; exit 1; }
 # Remove files created by pyinstaller
 del:
 	rm -rf ./dist/ ./build/ ./*.spec ./*.pyc ./*.log dass.spec dist/
--- build.sh
+++ build.sh
@@ -1,40 +1,44 @@
-#!/bin/bash
+#!/bin/bash -x
 #
 # Build and install:
 APPNAME="dass"
 #
-# Uses pyinstaller:
+# Check for the existence of the python3-venv package
+echo "Checking for python3-venv:"
+dpkg -l | grep -q python3-venv || echo "python3-venv package not installed'"
+
 
 # Check for the existence of the venv module
-if python3 -m venv --help &> /dev/null; then
-    echo "Python venv is installed."
-else
-    echo "Python venv is not installed!"
-    echo "If you are using Debian/Ubuntu/etc, install via:"
-    echo "sudo apt update; sudo apt install python3-venv"
-fi
+echo "Checking for venv module"
+python3 -m venv --help &> /dev/null || { echo "Python venv is not installed!'"; exit 1; }
+#    echo "Python venv is installed."
+#else
+#    echo "Python venv is not installed!"
+#    echo "If you are using Debian/Ubuntu/etc, install via:"
+#    echo "sudo apt update; sudo apt install python3-venv"
+#fi
 
 # Create a virtual environment if it doesn't exist
-if [ ! -d "venv" ]; then
-  python3 -m venv venv
-fi
+echo "Creating or reusing a virtual environment"
+[[ ! -d venv ]] && { python3 -m venv venv || { echo "venv creation failed" ; exit 1; } ; }
 
 # Activate the virtual environment
-source venv/bin/activate
-
-# Install the dependencies
-pip3 install -r requirements.txt
+echo "Activating the virtual environment"
+source venv/bin/activate || { echo "venv activation failed" ; exit 1; }
+echo "Installing dependenies"
+pip install -r requirements.txt || { echo "pip install failed" ; exit 1; }
 
 # Run PyInstaller to create the executable
-pyinstaller --onefile "$APPNAME".py --clean -F --noupx
+echo "Running PyInstaller"
+pyinstaller --onefile "$APPNAME".py --clean -F --noupx || { echo "pyinstaller failed" ; exit 1; }
 
-# Move the executable to /usr/local/bin
-# You might need to run this script as root to have permission for this operation
+# Copy/install the executable to /usr/local/bin
 echo "Installing to /usr/local/bin"
-sudo install -v -m 755 dist/"$APPNAME" /usr/local/bin/"$APPNAME"
+sudo install -v -m 755 dist/"$APPNAME" /usr/local/bin/"$APPNAME" || { echo "install failed" ; exit 1; }
 
 # Deactivate the virtual environment
-deactivate
+deactivate || { echo "venv deactivation failed" ; exit 1; }
+#deactivate
 
 # Remove the build files:
 rm -rfv ./dist/ ./build/ ./*.spec ./*.pyc ./*.log "$APPNAME".spec dist/ "$APPNAME"
